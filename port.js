63833151bfe66aee979642d8706ce0ab5ca41429
abrorbekx27 <133501107+abrorbekx27@users.noreply.github.com>
2023-05-13 04:12:32 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,8 @@
+var typed= new typed((".multiple-text");{
+  strings:["Brand Representative", "Sales Associate","Multilingual Candidate"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop:true
+}
+)
